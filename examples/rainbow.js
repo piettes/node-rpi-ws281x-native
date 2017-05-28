1c62ae2bdf6759cbd32d961cc671160567440ffa
Stéphane Piette <stephane.piette@codecentric.de>
2017-05-28 21:18:16 +0200

    edit LED Number

--- examples/rainbow.js
+++ examples/rainbow.js
@@ -1,6 +1,6 @@
 var ws281x = require('../lib/ws281x-native');
 
-var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
+var NUM_LEDS = parseInt(process.argv[2], 10) || 30 * 4 * 6,
     pixelData = new Uint32Array(NUM_LEDS);
 
 ws281x.init(NUM_LEDS);
